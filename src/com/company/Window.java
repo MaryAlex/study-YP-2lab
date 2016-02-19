c4be30114ef74933e5f3f65bb3ee1f4c3688320b
MaryAlex <worker96@mail.ru>
2016-02-19 20:59:35 +0300

    Work begin!

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,4 +1,11 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Window {
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
 }
