85fdc5e1b6126f5ac092a08c091f36c4964019b7
MaryAlex <worker96@mail.ru>
2016-02-23 02:10:12 +0300

    asymmetricKey ready

--- src/com/company/Info.java
+++ src/com/company/Info.java
@@ -1,27 +1,96 @@
 package com.company;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+
 
 public class Info {
+    public static final String HASH_ALGORITHM = "SHA-256";
+    public static final String ASYMMETRIC_ALGORITHM = "RSA";
     public String textField;
     public String hashText;
+    public String encryptedByAsymmetric;
+    PrivateKey privateKey;
+    PublicKey publicKey;
+
 
     public void textToHash() {
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
             messageDigest.reset();
             messageDigest.update(textField.getBytes());
 
             byte result[] = messageDigest.digest();
-            StringBuffer buf = new StringBuffer();
-            for (byte bytes : result) {
-                buf.append(String.format("%02x", bytes & 0xff));
-            }
-            hashText = buf.toString();
+            hashText = getHexadecimalValue(result);
             System.out.println(hashText);
         } catch (NoSuchAlgorithmException e1) {
             e1.getMessage();
+        }
+    }
+
+    private String getHexadecimalValue(byte[] result) {
+        StringBuffer buf = new StringBuffer();
+        for (byte bytes : result) {
+            buf.append(String.format("%02x", bytes & 0xff));
+        }
+        return buf.toString();
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+    
+    public void textAsymmetricKeyEncrypt() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ASYMMETRIC_ALGORITHM);
+            keyPairGenerator.initialize(1024);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            privateKey = keyPair.getPrivate();
+            publicKey = keyPair.getPublic();
+
+            Cipher cipher = Cipher.getInstance(ASYMMETRIC_ALGORITHM);
+
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            encryptedByAsymmetric = getHexadecimalValue(cipher.doFinal(textField.getBytes()));
+            System.out.println("Encrypted Data: " + encryptedByAsymmetric);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void textAsymmetricKeyDecrypt() {
+        try {
+            Cipher dipher = Cipher.getInstance(ASYMMETRIC_ALGORITHM);
+
+            dipher.init(Cipher.DECRYPT_MODE, privateKey);
+            System.out.println(new String(dipher.doFinal(hexStringToByteArray(encryptedByAsymmetric))));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -3,16 +3,15 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class Window extends JFrame {
     private JTextField textField;
     private JButton hashText;
-    private JButton button2;
+    private JButton asymmetricKeyButton;
     private JButton button3;
     private JButton button4;
     private JPanel panel;
+    private JTextArea textArea1;
     Info info = new Info();
 
     private ActionListener hashText() {
@@ -21,7 +20,15 @@ public class Window extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 info.textField = textField.getText();
                 info.textToHash();
-
+            }
+        };
+    }
+    private ActionListener asymmetricKey() {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                info.textField = textField.getText();
+                info.textAsymmetricKeyEncrypt();
             }
         };
     }
@@ -33,6 +40,7 @@ public class Window extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         hashText.addActionListener(hashText());
+        asymmetricKeyButton.addActionListener(asymmetricKey());
 
         setVisible(true);
     }
